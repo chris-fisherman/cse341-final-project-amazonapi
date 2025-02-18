40a7fa0c4dbafef51d28382074636c99f2a444fb
Nestor Otondo <oto23001@byui.edu>
2025-02-17 21:00:08 -0300

    Fixing validating put orders

--- routes/orders.js
+++ routes/orders.js
@@ -5,7 +5,7 @@ const { validateOrders } = require('../middlewares/validator');
 routes.get('/', orders.getAllOrders);
 routes.get('/:id', orders.getOrderById);
 routes.post('/', validateOrders, orders.createOrder);
-routes.put('/:id', validateOrders, orders.updateOrder);
+routes.put('/:id', orders.updateOrder);
 routes.delete('/:id', orders.deleteOrder);
 
 module.exports = routes;
